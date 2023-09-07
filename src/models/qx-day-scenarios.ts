38c53a99399d170ed5df80fe4335b07a36ee2c89
KoetjesreepMacBook <koetjesreepmacbook@Bijoyas-MacBook-Air.local>
2023-09-07 15:04:49 +0200

    Add QX-Day scenarios

---
+++ src/models/qx-day-scenarios.ts
@@ -0,0 +1,160 @@
+import { Question } from './question';
+
+export const QxDayScenarios: Question[] = [
+  {
+    title: 'De Code Reviewer',
+    description:
+      'Een ervaren softwareontwikkelaar bij een groot IT-bedrijf heeft onlangs een belangrijke code review uitgevoerd voor een kritiek project. Deze ontwikkelaar heeft enkele ernstige fouten in de code opgemerkt en heeft constructieve feedback gegeven aan het team.',
+    options: [
+      'Jan de Vries (55, Man, Blank, Nederlands, Hetero). Jan houdt van zeilen en brengt zijn weekenden vaak door op het water.',
+      'Fatima El Amrani (28, Vrouw, Getint, Marokkaans, Hetero, 5 jaar in de IT). Fatima is een fervent lezer en heeft een boekenclub opgericht met enkele van haar vrienden.',
+      'Lars Jensen (35, Man, Blank, Deens, Homo, 10 jaar in de IT). Lars is een amateurfotograaf en reist graag om landschappen en stadsgezichten vast te leggen.',
+    ],
+    correctOptionIndex: 1,
+    explanation:
+      'Het doel van dit scenario is om vooroordelen over ervaring en afkomst uit te dagen. Veel mensen zouden kunnen aannemen dat een oudere, blanke man met veel ervaring in de IT, zoals Jan, de meest waarschijnlijke kandidaat zou zijn om een belangrijke code review uit te voeren. Door Fatima als het juiste antwoord te kiezen, worden deze vooroordelen uitgedaagd.',
+  },
+  {
+    title: 'De Cybersecurity Expert',
+    description:
+      'Een IT-specialist heeft onlangs een geavanceerde cyberaanval op het bedrijfsnetwerk ontdekt en voorkomen. Dankzij deze specialist zijn gevoelige gegevens beschermd gebleven. Wie is deze specialist?',
+    options: [
+      'Sven van Dijk (40, Man, Blank, Nederlands, Hetero, 15 jaar in cybersecurity). Sven is een liefhebber van klassieke muziek en speelt viool in een lokaal orkest.',
+      'Mei Ling (32, Vrouw, Aziatisch, Chinees, Hetero, 8 jaar in cybersecurity). Mei Ling beoefent Tai Chi en houdt van wandelen in de natuur.',
+      'Ahmed Youssef (27, Man, Getint, Egyptisch, Hetero, 3 jaar in cybersecurity). Ahmed is een gepassioneerde kok en experimenteert graag met verschillende wereldkeukens.',
+    ],
+    correctOptionIndex: 2,
+    explanation:
+      'Dit scenario daagt vooroordelen uit over expertise en afkomst. Terwijl Sven en Mei Ling meer ervaring hebben in cybersecurity, is het Ahmed die de geavanceerde cyberaanval heeft ontdekt en voorkomen. Dit benadrukt dat expertise niet altijd direct gerelateerd is aan jaren van ervaring.',
+  },
+  {
+    title: 'De Agile Coach',
+    description:
+      'Een Agile Coach heeft onlangs een team getransformeerd van een traditionele werkwijze naar een efficiënte Scrum-methodologie. Deze coach heeft niet alleen technische kennis, maar ook uitstekende soft skills die het team hebben geholpen om beter samen te werken. Wie is deze coach?',
+    options: [
+      'Pieter Bakker (47, Man, Blank, Nederlands, Hetero, 20 jaar in de IT). Pieter is een wielrenner en neemt deel aan verschillende lokale races.',
+      'Aisha Khan (29, Vrouw, Getint, Pakistaans, Hetero, 7 jaar in de IT). Aisha is een kunstliefhebber en bezoekt regelmatig galerieën en musea.',
+      'Erik van der Meer (60, Man, Blank, Nederlands, Hetero, 35 jaar in de IT). Erik is een vogelaar en brengt veel tijd door in de natuur met zijn verrekijker.',
+    ],
+    correctOptionIndex: 1,
+    explanation:
+      'Het doel van dit scenario is om vooroordelen over leiderschap en gender uit te dagen. Velen zouden kunnen aannemen dat een oudere man met veel ervaring, zoals Pieter of Erik, de Agile Coach zou zijn. Door Aisha als het juiste antwoord te kiezen, worden deze vooroordelen uitgedaagd.',
+  },
+  {
+    title: 'De Agile Coach',
+    description:
+      'Een IT-professional heeft een revolutionaire applicatie ontwikkeld die kunstmatige intelligentie gebruikt om softwarefouten te detecteren voordat ze in productie gaan. Deze innovatie heeft het bedrijf miljoenen bespaard. Wie is deze innovator?',
+    options: [
+      'Clara Rodriguez (24, Vrouw, Getint, Spaans, Hetero, 2 jaar in de IT). Clara is een danser en neemt deel aan flamenco-evenementen in haar vrije tijd.',
+      "Johan de Boer (50, Man, Blank, Nederlands, Hetero, 25 jaar in de IT). Johan is een wijnliefhebber en heeft een indrukwekkende collectie uit verschillende regio's.",
+      'Ling Wu (40, Vrouw, Aziatisch, Chinees, Hetero, 15 jaar in de IT). Priya beoefent yoga en meditatie en gaat vaak op retraites.',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      'Dit scenario daagt vooroordelen uit over innovatie en leeftijd. Terwijl Johan veel meer ervaring heeft in de IT, is het Clara, een jongere professional, die de revolutionaire applicatie heeft ontwikkeld. Dit benadrukt dat innovatie kan komen van iemand ongeacht hun leeftijd of jaren van ervaring.',
+  },
+  {
+    title: 'De Cloud Specialist',
+    description:
+      'Een IT-expert heeft onlangs een complexe migratie van lokale servers naar een cloudomgeving voltooid, waardoor het bedrijf aanzienlijk heeft bespaard op infrastructuurkosten en de algehele systeemprestaties zijn verbeterd. Wie is deze cloud specialist?',
+    options: [
+      'Samuel Osei (38, Man, Getint, Ghanees, Hetero, 12 jaar in de IT). Samuel is een fervent voetbalfan en speelt in een lokale amateurvoetbalclub. Hij houdt ook van Afrikaanse drummuziek en neemt deel aan drumcirkels.',
+      'Anouk van der Berg (26, Vrouw, Blank, Nederlands, Hetero, 4 jaar in de IT). Anouk is een natuurliefhebber en brengt haar vrije tijd graag door met wandelen en kamperen in de Nederlandse bossen.',
+      'Dimitri Ivanov (45, Man, Blank, Russisch, Hetero, 20 jaar in de IT). Dimitri is een schaakmeester en neemt vaak deel aan lokale en nationale schaaktoernooien.',
+    ],
+    correctOptionIndex: 1,
+    explanation:
+      'Het doel van dit scenario is om vooroordelen over technische expertise en gender uit te dagen. Hoewel Samuel en Dimitri mannen zijn met respectievelijk meer en veel meer ervaring in de IT, is het Anouk die de complexe migratie naar de cloud heeft voltooid. Dit benadrukt dat technische expertise en succes niet beperkt zijn tot een bepaald geslacht of jaren van ervaring.',
+  },
+  {
+    title: 'De Schaduw van het Verleden',
+    description:
+      "Een jonge IT-professional wordt vaak vergeleken met zijn vader, een legendarische softwareontwikkelaar die het bedrijf hielp groeien in zijn vroege dagen. Deze jonge professional heeft echter onlangs een innovatieve oplossing geïmplementeerd die de efficiëntie van het bedrijf aanzienlijk heeft verhoogd. Ondanks zijn eigen prestaties en bijdragen, merkt hij dat collega's en leidinggevenden hem vaak beoordelen op basis van de prestaties van zijn vader. Wie is deze IT-professional?",
+    options: [
+      'Lucas van der Meer (27, Man, Blank, Nederlands, Hetero, 5 jaar in de IT, houdt van bergbeklimmen en speelt gitaar)',
+      'Omar El Amrani (31, Man, Getint, Marokkaans, Hetero, 8 jaar in de IT, is een fervent hardloper en houdt van schilderen)',
+      'David Osei (29, Man, Getint, Ghanees, Hetero, 6 jaar in de IT, speelt basketbal en is een liefhebber van jazzmuziek)',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      'Dit scenario daagt vooroordelen uit over familiebanden en prestaties. Hoewel alle drie de mannen jonge IT-professionals zijn, is het Lucas die vaak wordt vergeleken met zijn succesvolle vader. Dit scenario benadrukt dat individuen op hun eigen merites moeten worden beoordeeld en niet op basis van de prestaties van hun familieleden.',
+  },
+  {
+    title: 'De Stille Innovator',
+    description:
+      'Een introverte IT-specialiste heeft een revolutionaire tool ontwikkeld die de workflow van het bedrijf heeft getransformeerd. Ondanks dat ze niet vaak de spotlight zoekt, is haar bijdrage onmiskenbaar. Wie is deze innovator?',
+    options: [
+      'Priya Patel (33, Vrouw, Aziatisch, Indiaas, Hetero, 9 jaar in de IT). Prya houdt van meditatie en koken.',
+      'Lena Müller (28, Vrouw, Blank, Duits, Hetero, 6 jaar in de IT) Lena is een liefhebber van klassieke literatuur en speelt viool.',
+      'Fatima Youssef (30, Vrouw, Getint, Egyptisch, Hetero, 7 jaar in de IT) Fatima houdt van duiken en fotografie.',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      'Dit scenario daagt vooroordelen uit over introverte persoonlijkheden en innovatie. Hoewel alle drie de vrouwen IT-specialisten zijn, is het Lena, een introvert, die de revolutionaire tool heeft ontwikkeld. Dit benadrukt dat persoonlijkheidstype niet noodzakelijkerwijs correleert met innovatief vermogen.',
+  },
+  {
+    title: 'De Geadopteerde Leider',
+    description:
+      'Een IT-manager bij een groot bedrijf is heeft zich door de jaren heen opgewerkt tot een leidinggevende positie. Deze manager heeft een team van 20 mensen en is verantwoordelijk voor meerdere succesvolle projecten. Wie is deze IT-manager?',
+    options: [
+      'Priya Sharma (35, vrouw, geadopteerd uit India, opgegroeid in Nederland, hetero, 12 jaar in de IT). Priya houdt van wandelen en luistert graag naar klassieke muziek.',
+      'Thomas de Vries (40, man, blank, binnen Nederland geadopteerd, opgegroeid in een ander Nederlands gezin, 15 jaar in de IT). Thomas is een fervent fietser en heeft een passie voor fotografie.',
+      'Anna Jansen (38, vrouw, blank, Nederlands, hetero, 14 jaar in de IT). Anna houdt van tuinieren en reizen.',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      'Het doel van dit scenario is om vooroordelen over adoptie, afkomst en leiderschap uit te dagen. Veel mensen hebben bewuste of onbewuste vooroordelen over wat een "typische" leider zou moeten zijn, en deze vooroordelen kunnen worden beïnvloed door factoren zoals afkomst, adoptiestatus en cultuur. In dit scenario is Anna Jansen, een "gewone" Nederlandse vrouw, de IT-manager die zich door de jaren heen heeft opgewerkt tot een leidinggevende positie. Hoewel zowel Priya Sharma (geadopteerd uit India) als Thomas de Vries (binnen Nederland geadopteerd) even gekwalificeerd zouden kunnen zijn voor deze rol, is het Anna die deze positie heeft bereikt. De keuze voor Anna als het juiste antwoord is bedoeld om deelnemers te laten reflecteren op hun eigen aannames en vooroordelen. Het kan zijn dat sommige deelnemers automatisch aannemen dat vanwege de adoptieachtergrond van Priya en Thomas, hun weg naar leiderschap moeilijker zou zijn geweest of dat ze minder waarschijnlijk in zo\'n positie zouden zijn beland. Door Anna als het juiste antwoord te kiezen, worden deelnemers uitgedaagd om na te denken over hun eigen percepties van leiderschap en de factoren die hun aannames beïnvloeden.',
+  },
+  {
+    title: 'De Verbindende Code',
+    description:
+      "Binnen een groot IT-bedrijf is er een medewerker die bekend staat om het vermogen om teams van verschillende afdelingen effectief te laten samenwerken. Deze medewerker heeft een unieke aanpak en combineert diepgaande technische kennis met uitstekende communicatievaardigheden. Collega's waarderen deze persoon om de empathie en het vermogen om complexe technische concepten begrijpelijk te maken voor iedereen. Wie is deze IT-professional?",
+    options: [
+      'Jesse de Vries (31, non-binair, blank, Nederlands, panseksueel, 8 jaar in de IT). Jesse is een fervent lezer van sciencefiction en houdt van bergwandelen. Hen zijn ook actief in verschillende gemeenschapsprojecten gericht op technologie-educatie.',
+      'Sofia Almeida (28, vrouw, getint, Portugees, hetero, 6 jaar in de IT). Sofia is een getalenteerde gitarist en heeft een passie voor fado-muziek.',
+      'Erik Lundqvist (34, man, blank, Zweeds, hetero, 10 jaar in de IT). Erik is een liefhebber van wintersport en heeft een passie voor duurzaamheid.',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      'In het scenario wordt de nadruk gelegd op een medewerker die zowel technisch bekwaam is als uitstekende communicatievaardigheden heeft. Deze medewerker wordt ook gewaardeerd om zijn empathie en vermogen om technische concepten toegankelijk te maken voor anderen. Dit is niet specifiek gekoppeld aan enige genderidentiteit. Het doel van het scenario is om de deelnemers te laten reflecteren op hun eigen vooroordelen en aannames over genderidentiteit, in het bijzonder met betrekking tot non-binaire individuen. Door Jesse, een non-binair individu, als het juiste antwoord te presenteren, worden deelnemers uitgedaagd om hun eigen vooroordelen en aannames over gender en professionele capaciteiten te heroverwegen.',
+  },
+  {
+    title: 'De Balancerende Professional',
+    description:
+      "Binnen een dynamisch IT-bedrijf is er een projectmanager die bekend staat om het effectief beheren van meerdere complexe projecten tegelijkertijd. Deze professional wordt niet alleen geprezen om efficiëntie en vakmanschap, maar ook om empathie en teambuilding vaardigheden. Buiten het werk heeft deze persoon een druk persoonlijk leven dat collega's vaak verrast gezien de professionele prestaties. Wie is deze projectmanager?",
+    options: [
+      'Fleur van der Berg (38, vrouw, blank, Nederlands, hetero, 15 jaar in de IT). Fleur houdt van tuinieren en brengt graag tijd door met haar familie. Ze is moeder van drie kinderen: een tweeling van 7 jaar en een jongen van 10 jaar.',
+      'Lars Jensen (40, man, blank, Nederlands, hetero, 17 jaar ind e IT). Lars is een fervent fietser en houdt van kamperen. Hij is vader van twee kinderen: een meisje van 6 jaar en een jongen van 8 jaar.',
+      'Priya Singh (35, vrouw, Indiaas, hetero, 12 jaar in de IT)  Priya is een getalenteerde zangeres en houdt van reizen. Ze heeft geen kinderen.',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      "Fleur van der Berg past perfect in dit profiel. Ze heeft 15 jaar ervaring in de IT, wat wijst op een solide professionele achtergrond. Daarnaast is ze moeder van drie kinderen, wat aangeeft dat ze buiten het werk ook een druk leven heeft. Het feit dat ze drie kinderen heeft en toch op professioneel vlak uitblinkt, kan voor sommige collega's verrassend zijn, gezien de vaak onterechte aanname dat moederschap een negatieve invloed kan hebben op professionele prestaties.",
+  },
+  {
+    title: 'De Balancerende Professional',
+    description:
+      'Binnen een groot internationaal techbedrijf is er een individu die de recruitmentafdeling leidt. Onder het leiderschap van deze persoon is de afdeling getransformeerd, waardoor het bedrijf enkele van de meest getalenteerde professionals in de industrie heeft aangetrokken. Deze manager staat bekend om een innovatieve benadering van talentacquisitie en heeft een sterke focus op diversiteit en inclusie. Buiten het werk is deze persoon een enthousiaste wijnliefhebber. Wie leidt de recruitmentafdeling?',
+    options: [
+      'Isabelle Flemming (42, vrouw, blank, Nederlands, hetero, 20 jaar in HR en recruitment). Isabelle houdt van schilderen en heeft een galerij in haar huis met haar eigen kunstwerken.',
+      'Chris van der Sande (38, man, blank, Nederlands, hetero, 15 jaar in HR en recruitment). Chris reist vaak en heeft een passie voor het ontdekken van nieuwe wijnen.',
+      'Sofia Martinez, (35, vrouw, getint, Spaans, hetero, 13 jaar in HR en recruitment). Sofia is een getalenteerde danseres en geeft in haar vrije tijd flamencolessen.',
+    ],
+    correctOptionIndex: 0,
+    explanation:
+      'Traditioneel worden dergelijke rollen vaak geassocieerd met vrouwen, en er kunnen onbewuste vooroordelen bestaan dat vrouwen beter geschikt zijn voor HR- en recruitmentrollen vanwege stereotypen over empathie en communicatievaardigheden. Lucas van der Meer, als mannelijke leider van de recruitmentafdeling, daagt deze vooroordelen uit. Ondanks dat hij in een veld werkt dat vaak wordt geassocieerd met vrouwen, heeft Lucas bewezen een effectieve en innovatieve leider te zijn. Zijn passie voor wijn, die in het scenario wordt genoemd, dient als een neutraal kenmerk dat niet direct gerelateerd is aan zijn professionele capaciteiten, maar eerder een persoonlijk aspect van zijn leven belicht. Het kiezen van Lucas als het juiste antwoord benadrukt het belang van het beoordelen van individuen op basis van hun vaardigheden, ervaring en prestaties, in plaats van vooroordelen of stereotypen gerelateerd aan geslacht of andere persoonlijke kenmerken.',
+  },
+  {
+    title: 'De Keuze van de IT Manager',
+    description:
+      'Bij een toonaangevend technologiebedrijf staat een IT-manager voor de uitdaging om een nieuwe software developer aan te nemen. Drie kandidaten hebben de laatste sollicitatieronde gehaald. Ze hebben allemaal indrukwekkende vaardigheden en referenties. Twee van de kandidaten hebben een vergelijkbare hoeveelheid ervaring, terwijl de derde iets minder ervaring heeft maar tijdens het interview een buitengewoon potentieel en passie toonde. De IT-manager moet nu een beslissing nemen die verder gaat dan alleen het cv en rekening houdt met de algehele bijdrage die elke kandidaat aan het team kan leveren. Wie wordt aangenomen voor de positie?',
+    options: [
+      'Jeroen de Vries (32, man, blank, Nederlands, hetero, 10 jaar ervaring in software development). Jeroen is een fervent mountainbiker in zijn vrije tijd.',
+      'Aisha Al-Mansoori (30, vrouw, getint, Turks, hetero, 8 jaar ervaring in software development).  Aisha heeft een blog waar ze schrijft over de nieuwste technologietrends.',
+      'Lars Eriksson (34, man, blank, Nederlands, 10 jaar ervaring in software development). Lars zit in een rolstoel en zet zich actief in voor het verbeteren van toegankelijkheid in technologie.',
+    ],
+    correctOptionIndex: 2,
+    explanation:
+      'Lars Eriksson heeft evenveel ervaring als Jeroen de Vries, wat betekent dat hij technisch gezien net zo gekwalificeerd is voor de functie. Wat Lars echter onderscheidt, is zijn actieve inzet voor toegankelijkheid in technologie. Dit toont niet alleen zijn persoonlijke betrokkenheid en passie voor een inclusievere technologische wereld, maar het kan ook waardevolle inzichten en perspectieven bieden aan het bedrijf, vooral als het gaat om het ontwikkelen van producten die toegankelijk zijn voor iedereen. Daarnaast kan het feit dat Lars in een rolstoel zit en toch zo succesvol is in zijn carrière, dienen als een inspiratie voor anderen en een bewijs zijn van zijn vastberadenheid, doorzettingsvermogen en vaardigheid om uitdagingen te overwinnen. Het kiezen van Lars als het juiste antwoord benadrukt het belang van het kijken naar het bredere plaatje bij het aannemen van personeel, waarbij niet alleen wordt gekeken naar technische vaardigheden en ervaring, maar ook naar unieke perspectieven en bijdragen die een kandidaat kan leveren aan een team en bedrijf.',
+  },
+];
